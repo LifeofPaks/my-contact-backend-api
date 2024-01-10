ab60108b4708cd51d0aad0944f1c4f826e19f8b2
Paks <promise.chukwudi@yahoo.com>
2024-01-10 15:46:15 +0100

    updated

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,43 +1,50 @@
+const asyncHandler =  require('express-async-handler')
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Get contacts
 //@route GET /api/contacts/:id
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Create contact
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
+    const {name, email, phone} = req.body
+    if(!name || !email || !phone){
+        res.status(400)
+        throw new Error('All fields are mandatory!')
+    }
     console.log(`The request body is: ${JSON.stringify(req.body)}`)
   res.status(200).json({ message: "Create contact" });
-};
+});
 
 //@desc Update contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 //@desc Delete contacts
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5007;
 
-app.use(express.json()) //Body parser middleware
+app.use(express.json()) //Body parser inbuild middleware
 app.use("/api/contacts", require("./routes/contactRoutes")); //This is the middleware
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
