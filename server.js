371c0b9b91b5a7341a4ccde3b7e48ddb078f6bf1
Paks <promise.chukwudi@yahoo.com>
2024-01-11 15:26:23 +0100

    updated

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5007;
 
+connectDb()
 app.use(express.json()) //Body parser inbuild middleware
 app.use("/api/contacts", require("./routes/contactRoutes")); //This is the middleware
-app.use(errorHandler)
+app.use(errorHandler) //Custom middleware
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
