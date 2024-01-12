d910de0e6ee3df442b836d7c0c683c7f6cf01e42
Paks <promise.chukwudi@yahoo.com>
2024-01-12 22:20:31 +0100

    updated

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,11 +1,13 @@
-const asyncHandler =  require('express-async-handler')
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 //@desc Get contacts
@@ -13,7 +15,12 @@ const getContacts = asyncHandler(async (req, res) => {
 //@access public
 
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Create contact
@@ -21,13 +28,18 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 
 const createContact = asyncHandler(async (req, res) => {
-    const {name, email, phone} = req.body
-    if(!name || !email || !phone){
-        res.status(400)
-        throw new Error('All fields are mandatory!')
-    }
-    console.log(`The request body is: ${JSON.stringify(req.body)}`)
-  res.status(200).json({ message: "Create contact" });
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(200).json(contact);
 });
 
 //@desc Update contacts
@@ -35,7 +47,19 @@ const createContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contacts
@@ -43,7 +67,14 @@ const updateContact = asyncHandler(async (req, res) => {
 //@access public
 
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+
+    await Contact.remove()
+  res.status(200).json(contact);
 });
 
 module.exports = {
--- models/contactModel.js
+++ models/contactModel.js
@@ -1,1 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add the contact name"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please add the contact Email address"],
+    },
+
+    phone: {
+      type: String,
+      required: [true, "Please add the contact Phone number"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
